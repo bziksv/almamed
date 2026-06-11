ae4f8196ff7011d2817af64b2584ca6e8def5e2f
Станислав <stanislav@MacBook-Air-Stanislav-2.local>
2026-06-11 18:30:32 +0300

    updated

--- wa-apps/shop/plugins/slider/lib/actions/shopSliderPluginBackendSave.controller.php
+++ wa-apps/shop/plugins/slider/lib/actions/shopSliderPluginBackendSave.controller.php
@@ -12,53 +12,72 @@ class shopSliderPluginBackendSaveController extends waJsonController
         $img_path = waRequest::post('img_path');
 
         $files = array();
-        foreach($_FILES['img']['error'] as $key => $img){
+        foreach ($_FILES['img']['error'] as $key => $img) {
             if ($img) {
-                if($img_path[$key]){
-                    $files[$key] = $img_path[$key];
-                }else{
-                    $files[$key] = $img;
+                $files[$key] = ifset($img_path, $key, '');
+                if ($files[$key]) {
+                    $this->ensureMobileThumbnail($files[$key]);
                 }
-            }
-            else {
-                $format = array('/png','/jpeg');
+            } else {
+                $format = array('/png', '/jpeg');
 
-                if(in_array(strstr($_FILES['img']['type'][$key],'/'), $format)){
+                if (in_array(strstr($_FILES['img']['type'][$key], '/'), $format)) {
                     $file_name = $_FILES['img']['name'][$key];
                     $files[$key] = '/wa-data/public/shop/slider/img/' . $file_name;
-                    $files_root = $_SERVER['DOCUMENT_ROOT'].$files[$key];
+                    $files_root = wa()->getConfig()->getPath('data') . '/public/shop/slider/img/' . $file_name;
 
-                    waFiles::create(wa()->getConfig()->getPath('data').'/public/shop/slider/img');
+                    waFiles::create(wa()->getConfig()->getPath('data') . '/public/shop/slider/img');
 
                     $image = waImage::factory($_FILES['img']['tmp_name'][$key]);
                     $image->save($files_root, 100);
 
-                    $image_sm = waImage::factory($_FILES['img']['tmp_name'][$key]);
-                    $image_sm->resize(576, 220, 'WIDTH');
-                    $image_sm->save(str_replace('/img/','/img/sm_',$files_root), 100);
-
-                }else{
+                    $this->saveMobileThumbnail($_FILES['img']['tmp_name'][$key], $files_root);
+                } else {
                     $files[$key] = false;
                 }
             }
         }
 
         $records = $model->order('sort ASC')->fetchAll();
-        foreach($records as $r){
+        foreach ($records as $r) {
             $model->deleteById($r['id']);
         }
 
-        foreach($files as $key => $db){
-
+        foreach ($files as $key => $db) {
             $model->insert(array(
-                "sort" => $sort[$key],
-                "link" => $link[$key],
-                "alt" => $alt[$key],
-                "img" => $db
+                'sort' => $sort[$key],
+                'link' => $link[$key],
+                'alt' => $alt[$key],
+                'img' => $db,
             ));
         }
 
         $this->redirect('/webasyst/shop/?action=plugins#/slider/');
     }
 
+    protected function saveMobileThumbnail($source, $files_root)
+    {
+        $image_sm = waImage::factory($source);
+        $image_sm->resize(576, 220, 'WIDTH');
+        $image_sm->save($this->mobilePath($files_root), 100);
+    }
+
+    protected function ensureMobileThumbnail($public_path)
+    {
+        $basename = basename($public_path);
+        $files_root = wa()->getConfig()->getPath('data') . '/public/shop/slider/img/' . $basename;
+        if (!file_exists($files_root)) {
+            return;
+        }
+        $mobile_path = $this->mobilePath($files_root);
+        if (file_exists($mobile_path)) {
+            return;
+        }
+        $this->saveMobileThumbnail($files_root, $files_root);
+    }
+
+    protected function mobilePath($files_root)
+    {
+        return str_replace('/img/', '/img/sm_', $files_root);
+    }
 }
--- wa-apps/shop/plugins/slider/lib/shopSliderPlugin.class.php
+++ wa-apps/shop/plugins/slider/lib/shopSliderPlugin.class.php
@@ -2,22 +2,36 @@
 
 class shopSliderPlugin extends shopPlugin
 {
-
-
-    static public function slider(){
-
+    static public function slider()
+    {
         $model = new shopSliderModel();
         $records = $model->order('sort ASC')->fetchAll();
+        foreach ($records as &$slide) {
+            $slide['img_mobile'] = self::getSlideMobileImage(ifset($slide, 'img', ''));
+        }
+        unset($slide);
+
         $app_config = wa()->getConfig()->getAppConfig('shop');
         $view = wa()->getView();
-		
         $view->assign('slides', $records);
-        return $view->fetch($app_config->getPluginPath('slider').'/templates/frontendSlider.html');
 
+        return $view->fetch($app_config->getPluginPath('slider').'/templates/frontendSlider.html');
     }
 
+    /**
+     * Mobile banner: sm_* thumbnail generated on upload (576px wide).
+     */
+    protected static function getSlideMobileImage($img)
+    {
+        if (!$img) {
+            return '';
+        }
+        $basename = basename($img);
+        $mobile_path = wa()->getConfig()->getPath('data').'/public/shop/slider/img/sm_'.$basename;
+        if (file_exists($mobile_path)) {
+            return '/wa-data/public/shop/slider/img/sm_'.$basename;
+        }
 
+        return $img;
+    }
 }
-
-
-
